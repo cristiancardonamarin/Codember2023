ca0c9cf0a63d4e3f2297758394d31ef7c6b35a5b
Cristian Cardona Marin <cristiancardonamarin@outlook.com>
2023-11-05 15:33:57 +0100

    Challenge 01

---
+++ Codember/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+var httpClient = new HttpClient();
+var response = await httpClient.GetAsync("https://codember.dev/data/message_01.txt");
+var message = await response.Content.ReadAsStringAsync();
+message = message.Trim();
+
+var words = message.Split(" ");
+var groups = words.GroupBy(word => word);
+var finalResult = string.Join("",groups.Select(group => $"{group.Key}{group.Count()}"));
+
+Console.WriteLine(finalResult);
+Console.ReadKey();
